9d34b4126ff1c8873465e8974457f4f3f32afc15
이동석 ezpmp 연구원 <dslee@ezpmp.co.kr>
2025-07-08 18:00:34 +0900

    [feat] 페이지 미리 가져오기 기능 추가 및 링크 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,6 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
   const onClickButton = () => {
     router.push("/book/123");
   };
+  useEffect(() => {
+    router.prefetch("/book/1232324");
+  }, []);
+
   return (
     <>
       <header>글로벌 헤더</header>
@@ -19,6 +24,9 @@ export default function App({ Component, pageProps }: AppProps) {
         &nbsp;
         <Link href="/search">search</Link>
         &nbsp;
+        <Link href="/search" prefetch={false}>
+          search/123
+        </Link>
         <div>
           <button onClick={onClickButton}>book/123 으로 가는 버튼</button>
         </div>
