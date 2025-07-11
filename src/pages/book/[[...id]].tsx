d02106459ceaef62194917001c89e2194929efd6
이동석 ezpmp 연구원 <dslee@ezpmp.co.kr>
2025-07-11 10:59:16 +0900

    [feat] API 연동 테스트  section2-ch12

--- src/pages/book/[[...id]].tsx
+++ src/pages/book/[[...id]].tsx
@@ -1,8 +1,27 @@
 import { useRouter } from "next/router";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchOneBook from "@/pages/lib/fetch-one-book";
 
-export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
-  console.log("id : ", id);
-  return <div>북 페이지 {id}</div>;
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // const { id } = context.params;
+  // console.log("id : ", id);
+  const bookId = Array.isArray(context.params?.id)
+    ? parseInt(context.params.id[0])
+    : parseInt(context.params?.id || "1");
+  const bookData = await fetchOneBook(bookId);
+  console.log("bookData : ", bookData);
+  return {
+    props: { bookData },
+  };
+};
+
+export default function Page({
+  bookData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // const router = useRouter();
+  // const { id } = router.query;
+  // console.log("id : ", id);
+  return <div>북 페이지 {bookData.title}</div>;
 }
